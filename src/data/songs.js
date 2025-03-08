0215342c67ea02b3342382c6237307bfd86df5d1
Isaiah Aganon <isaie@dhcp-51-232.cu-wireless-faculty.carleton.ca>
2025-03-08 16:52:00 -0500

    more songs

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,7 +1,9 @@
 export const songs = [
-    "adeus.wav",
-    "birdseyeview.wav",
-    "fipt1.wav",
+    "trizNate1.wav",
+    "trizNate2.wav",
     "fipt2.wav",
+    "fipt1.wav",
     "fipt3.wav",
+    "foodgrade.wav",
+    "marie.wav",
 ]
