55665c92b916db6834568e4c909390a37f55c327
AganonIsaiah <isaiahaganon0@gmail.com>
2025-05-23 15:29:29 -0400

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://isaiahaganon.vercel.app/', 
+  generateRobotsTxt: true,           
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 7000,
+};
