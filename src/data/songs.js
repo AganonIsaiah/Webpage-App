43609078574493683710d77325a756d5dbc822d3
AganonIsaiah <isaiahaganon0@gmail.com>
2025-05-24 15:27:23 -0400

    fixing blur v2

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,6 +1,7 @@
 const url = '/songs-dryen'
 
 export const songs = [
+    { file: `${url}/nothingisfree.mp3`},
     { file: `${url}/day110.mp3`},
     { file: `${url}/mournatnight.mp3`},
     { file: `${url}/needall.mp3`},
