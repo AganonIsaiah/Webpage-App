ab6796463f09f7202f18cf4f3e5887c117269c27
AganonIsaiah <isaiahaganon0@gmail.com>
2025-05-24 22:06:51 -0400

    new songs

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,8 +1,8 @@
 const url = '/songs-dryen'
 
 export const songs = [
+     { file: `${url}/nothingisfree.mp3`},
     { file: `${url}/day110.mp3`},
-    { file: `${url}/nothingisfree.mp3`},
     { file: `${url}/mournatnight.mp3`},
     { file: `${url}/needall.mp3`},
     { file: `${url}/brotherskeeper.mp3`},
