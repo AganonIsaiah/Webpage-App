5509f9df8f7e4ecc377485a78c534f2c8ab494d8
AganonIsaiah <isaiahaganon0@gmail.com>
2025-05-25 12:37:26 -0400

    optimizing seo w/ full name

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,13 @@ import { Analytics } from '@vercel/analytics/react';
 import Head from 'next/head';
 
 export const metadata = {
-  title: "Isaiah's Webpage",
+  title: "Isaiah Aganon's Webpage",
   description: "Portfolio and projects by Isaiah Aganon, Computer Science student at Carleton University.",
   icons: {
     icon: "/images/en.png"
   },
   openGraph: {
-    title: "Isaiah's Webpage",
+    title: "Isaiah Aganon's Webpage",
     description: "Portfolio and projects by Isaiah Aganon, Computer Science student at Carleton University.",
     url: "https://isaiahaganon.vercel.app/",
     siteName: "Isaiah Aganon Portfolio",
